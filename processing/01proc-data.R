725c8370060f37c3561c8f068affb5fcfc7c2809
Andreas-Lafferte <andreas.lafferte.tamayo@gmail.com>
2021-01-15 16:55:27 -0300

    Changes

--- processing/01proc-data.R
+++ processing/01proc-data.R
@@ -94,61 +94,61 @@ ene9$b14_rev4cl_caenes <- as_factor(ene9$b14_rev4cl_caenes)
 # 4.1. Recodificar rama actividad económica ---- 
 
 # 2010
-ene0 <- ene0 %>% mutate(b14=case_when(b14==1 ~,
-                                      b14==2 ~,
-                                      b14==3 ~,
-                                      b14==4 ~,
-                                      b14==5 ~,
-                                      b14==6 ~,
-                                      b14==7 ~,
-                                      b14==8 ~,
-                                      b14==9 ~,
-                                      b14==10 ~,
-                                      b14==11 ~,
-                                      b14==12 ~,
-                                      b14==13 ~,
-                                      b14==14 ~,
-                                      b14==15 ~
-                                      b14==16 ~,
-                                      b14==17 ~,))
+ene0 <- ene0 %>% mutate(b14=case_when(b14==1 ~ "Agricultura y pesca",
+                                      b14==2 ~ "Agricultura y pesca",
+                                      b14==3 ~ "Minería",
+                                      b14==4 ~ "Industria",
+                                      b14==5 ~ "Electricidad,  gas  y  agua",
+                                      b14==6 ~ "Construcción",
+                                      b14==7 ~ "Comercio",
+                                      b14==8 ~ "Comercio",
+                                      b14==9 ~ "Transporte y comunicaciones",
+                                      b14==10 ~ "Establecimientos financieros",
+                                      b14==11 ~ "Establecimientos financieros",
+                                      b14==12 ~ "Servicios",
+                                      b14==13 ~ "Servicios",
+                                      b14==14 ~ "Servicios",
+                                      b14==15 ~ "Servicios",
+                                      b14==16 ~ "Servicios",
+                                      b14==17 ~ "Servicios"))
 
 # 2011
-ene1 <- ene1 %>% mutate(b14=case_when(b14==1 ~,
-                                      b14==2 ~,
-                                      b14==3 ~,
-                                      b14==4 ~,
-                                      b14==5 ~,
-                                      b14==6 ~,
-                                      b14==7 ~,
-                                      b14==8 ~,
-                                      b14==9 ~,
-                                      b14==10 ~,
-                                      b14==11 ~,
-                                      b14==12 ~,
-                                      b14==13 ~,
-                                      b14==14 ~,
-                                      b14==15 ~
-                                      b14==16 ~,
-                                      b14==17 ~,))
+ene1 <- ene1 %>% mutate(b14=case_when(b14==1 ~ "Agricultura y pesca",
+                                      b14==2 ~ "Agricultura y pesca",
+                                      b14==3 ~ "Minería",
+                                      b14==4 ~ "Industria",
+                                      b14==5 ~ "Electricidad,  gas  y  agua",
+                                      b14==6 ~ "Construcción",
+                                      b14==7 ~ "Comercio",
+                                      b14==8 ~ "Comercio",
+                                      b14==9 ~ "Transporte y comunicaciones",
+                                      b14==10 ~ "Establecimientos financieros",
+                                      b14==11 ~ "Establecimientos financieros",
+                                      b14==12 ~ "Servicios",
+                                      b14==13 ~ "Servicios",
+                                      b14==14 ~ "Servicios",
+                                      b14==15 ~ "Servicios",
+                                      b14==16 ~ "Servicios",
+                                      b14==17 ~ "Servicios"))
 
 # 2012 
-ene2 <- ene2 %>% mutate(b14=case_when(b14==1 ~,
-                                      b14==2 ~,
-                                      b14==3 ~,
-                                      b14==4 ~,
-                                      b14==5 ~,
-                                      b14==6 ~,
-                                      b14==7 ~,
-                                      b14==8 ~,
-                                      b14==9 ~,
-                                      b14==10 ~,
-                                      b14==11 ~,
-                                      b14==12 ~,
-                                      b14==13 ~,
-                                      b14==14 ~,
-                                      b14==15 ~
-                                      b14==16 ~,
-                                      b14==17 ~,))
+ene2 <- ene2 %>% mutate(b14=case_when(b14==1 ~ "Agricultura y pesca",
+                                      b14==2 ~ "Agricultura y pesca",
+                                      b14==3 ~ "Minería",
+                                      b14==4 ~ "Industria",
+                                      b14==5 ~ "Electricidad,  gas  y  agua",
+                                      b14==6 ~ "Construcción",
+                                      b14==7 ~ "Comercio",
+                                      b14==8 ~ "Comercio",
+                                      b14==9 ~ "Transporte y comunicaciones",
+                                      b14==10 ~ "Establecimientos financieros",
+                                      b14==11 ~ "Establecimientos financieros",
+                                      b14==12 ~ "Servicios",
+                                      b14==13 ~ "Servicios",
+                                      b14==14 ~ "Servicios",
+                                      b14==15 ~ "Servicios",
+                                      b14==16 ~ "Servicios",
+                                      b14==17 ~ "Servicios"))
 
 # 2013 
 ene3 <- ene3 %>% mutate(b14_rev4cl_caenes=case_when(b14_rev4cl_caenes==1 ~ "Agricultura y pesca",
@@ -318,8 +318,56 @@ ene9 <- ene9 %>% mutate(b14_rev4cl_caenes=case_when(b14_rev4cl_caenes==1 ~ "Agri
 # 4.2. Diseño muestral ----
 
 # 2010
+ene0 %>% filter(b5==2 | b5==3 & (b2==2|b2==3)) %>% select(fact_cal) %>% sum() # CÁLCULO SIN AGRUPAR POR RAMA
+
+ene0_pond <- ene0  %>% as_survey_design(ids = 1, strata = estrato, weights = fact_cal) # FACTORES DE EXPANSION
+
+h <- ene0_pond %>% 
+  group_by(activ) %>%  filter(b5==2 | b5==3 & (b2==2|b2==3)) %>%
+  summarize(frecuencias = survey_total(na.rm = T),
+            proporcion = survey_mean(vartype = "ci", level = 0.95, na.rm = TRUE),
+            n = unweighted(n())) # COMPROBAR 
+
+h <- ene0_pond %>% 
+  group_by(b14) %>%  filter(b5==2 | b5==3 & (b2==2|b2==3)) %>%
+  summarize(frecuencias = survey_total(na.rm = T),
+            proporcion = survey_mean(vartype = "ci", level = 0.95, na.rm = TRUE),
+            n = unweighted(n())) # CÁLCULO AGRUPANDO POR RAMAS
+
 # 2011
+ene1 %>% filter(b5==2 | b5==3 & (b2==2|b2==3)) %>% select(fact_cal) %>% sum() # CÁLCULO SIN AGRUPAR POR RAMA
+
+ene1_pond <- ene1  %>% as_survey_design(ids = 1, strata = estrato, weights = fact_cal) # FACTORES DE EXPANSION
+
+i <- ene1_pond %>% 
+  group_by(activ) %>%  filter(b5==2 | b5==3 & (b2==2|b2==3)) %>%
+  summarize(frecuencias = survey_total(na.rm = T),
+            proporcion = survey_mean(vartype = "ci", level = 0.95, na.rm = TRUE),
+            n = unweighted(n())) # COMPROBAR 
+
+i <- ene1_pond %>% 
+  group_by(b14) %>%  filter(b5==2 | b5==3 & (b2==2|b2==3)) %>%
+  summarize(frecuencias = survey_total(na.rm = T),
+            proporcion = survey_mean(vartype = "ci", level = 0.95, na.rm = TRUE),
+            n = unweighted(n())) # CÁLCULO AGRUPANDO POR RAMAS
+
 # 2012
+ene2 %>% filter(b5==2 | b5==3 & (b2==2|b2==3)) %>% select(fact_cal) %>% sum() # CÁLCULO SIN AGRUPAR POR RAMA
+
+ene2_pond <- ene2  %>% as_survey_design(ids = 1, strata = estrato, weights = fact_cal) # FACTORES DE EXPANSION
+
+j <- ene2_pond %>% 
+  group_by(activ) %>%  filter(b5==2 | b5==3 & (b2==2|b2==3)) %>%
+  summarize(frecuencias = survey_total(na.rm = T),
+            proporcion = survey_mean(vartype = "ci", level = 0.95, na.rm = TRUE),
+            n = unweighted(n())) # COMPROBAR 
+
+j <- ene2_pond %>% 
+  group_by(b14) %>%  filter(b5==2 | b5==3 & (b2==2|b2==3)) %>%
+  summarize(frecuencias = survey_total(na.rm = T),
+            proporcion = survey_mean(vartype = "ci", level = 0.95, na.rm = TRUE),
+            n = unweighted(n())) # CÁLCULO AGRUPANDO POR RAMAS
+
 # 2013
 ene3 %>% filter(b5==2 | b5==3 & (b2==2|b2==3)) %>% select(fact_cal) %>% sum() # CÁLCULO SIN AGRUPAR POR RAMA
 
@@ -449,7 +497,10 @@ d <- d[-c(3,4,5,6,7)]
 e <- e[-c(3,4,5,6,7)]
 f <- f[-c(3,4,5,6,7)]
 g <- g[-c(3,4,5,6,7)] 
-# FALTAN AÑOS 2010 A 2012 
+# AÑOS 2010 A 2012 
+h <- h[-c(3,4,5,6,7)]
+i <- i[-c(3,4,5,6,7)]
+j <- j[-c(3,4,5,6,7)]
 
 # 5.2. Names ---- 
 names(a)<-c("Rama.Actividad.Económica","2013")
@@ -459,6 +510,10 @@ names(d)<-c("Rama.Actividad.Económica","2016")
 names(e)<-c("Rama.Actividad.Económica","2017")
 names(f)<-c("Rama.Actividad.Económica","2018")
 names(g)<-c("Rama.Actividad.Económica","2019")
+# AÑOS 2010 A 2012
+names(h)<-c("Rama.Actividad.Económica","2010")
+names(i)<-c("Rama.Actividad.Económica","2011")
+names(j)<-c("Rama.Actividad.Económica","2012")
 
 # 5.3. Merge ----
 cuadro2013_2014 <- merge(a, b, by = "Rama.Actividad.Económica", all.x = T)
@@ -467,5 +522,23 @@ cuadro2017_2018 <- merge(e, f, by = "Rama.Actividad.Económica", all.x = T)
 cuadro2013_2019 <- merge(cuadro2013_2014, cuadro2015_2016, by = "Rama.Actividad.Económica", all.x = T)
 cuadro2013_2019 <- merge(cuadro2013_2019, cuadro2017_2018, by = "Rama.Actividad.Económica", all.x = T)
 cuadro2013_2019 <- merge(cuadro2013_2019, g, by = "Rama.Actividad.Económica", all.x = T)
+# AÑOS 2010 A 2012
+cuadro2010_2011 <- merge(h, i, by = "Rama.Actividad.Económica", all.x = T)
+cuadro2010_2012 <- merge(cuadro2010_2011, j, by = "Rama.Actividad.Económica", all.x = T)
+cuadro2010_2019 <- merge(cuadro2010_2012, cuadro2013_2019, by = "Rama.Actividad.Económica", all.x = T)
+
+cuadro2010_2019<- cuadro2010_2019 %>% add_row(Rama.Actividad.Económica="Total",
+                                   `2010`=sum(.$`2010`[1]+.$`2010`[2]+.$`2010`[3]+.$`2010`[4]+.$`2010`[5]+.$`2010`[6]+.$`2010`[7]+.$`2010`[8]+.$`2010`[9]),         
+                                   `2011`=sum(.$`2011`[1]+.$`2011`[2]+.$`2011`[3]+.$`2011`[4]+.$`2011`[5]+.$`2011`[6]+.$`2011`[7]+.$`2011`[8]+.$`2011`[9]),
+                                   `2012`=sum(.$`2012`[1]+.$`2012`[2]+.$`2012`[3]+.$`2012`[4]+.$`2012`[5]+.$`2012`[6]+.$`2012`[7]+.$`2012`[8]+.$`2012`[9]),
+                                   `2013`=sum(.$`2013`[1]+.$`2013`[2]+.$`2013`[3]+.$`2013`[4]+.$`2013`[5]+.$`2013`[6]+.$`2013`[7]+.$`2013`[8]+.$`2013`[9]),
+                                   `2014`=sum(.$`2014`[1]+.$`2014`[2]+.$`2014`[3]+.$`2014`[4]+.$`2014`[5]+.$`2014`[6]+.$`2014`[7]+.$`2014`[8]+.$`2014`[9]),
+                                   `2015`=sum(.$`2015`[1]+.$`2015`[2]+.$`2015`[3]+.$`2015`[4]+.$`2015`[5]+.$`2015`[6]+.$`2015`[7]+.$`2015`[8]+.$`2015`[9]),
+                                   `2016`=sum(.$`2016`[1]+.$`2016`[2]+.$`2016`[3]+.$`2016`[4]+.$`2016`[5]+.$`2016`[6]+.$`2016`[7]+.$`2016`[8]+.$`2016`[9]),
+                                   `2017`=sum(.$`2017`[1]+.$`2017`[2]+.$`2017`[3]+.$`2017`[4]+.$`2017`[5]+.$`2017`[6]+.$`2017`[7]+.$`2017`[8]+.$`2017`[9]),
+                                   `2018`=sum(.$`2018`[1]+.$`2018`[2]+.$`2018`[3]+.$`2018`[4]+.$`2018`[5]+.$`2018`[6]+.$`2018`[7]+.$`2018`[8]+.$`2018`[9]),
+                                   `2019`=sum(.$`2019`[1]+.$`2019`[2]+.$`2019`[3]+.$`2019`[4]+.$`2019`[5]+.$`2019`[6]+.$`2019`[7]+.$`2019`[8]+.$`2019`[9]))
 
 # 6. Export ----
+save(cuadro2010_2019, file= "input/cuadro2010_2019.RData")
+write_xlsx(cuadro2010_2019,"output/cuadro2010_2019.xlsx", col_names = TRUE,format_headers = TRUE)
